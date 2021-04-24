bb415d029f64fbe2b3214500c28cfca7590190fa
Mike <15151851516@163.com>
2021-04-24 22:53:11 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,85 @@
 <template>
-  <div>
-    <p>Money</p>
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+    </div>
     <Nav/>
   </div>
 </template>
@@ -17,5 +96,14 @@ export default class Money extends Vue {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
