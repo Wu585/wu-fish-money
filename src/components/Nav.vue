f517c9e0cd7baab5d2ed11b746f2cd817e39d89e
Mike <15151851516@163.com>
2021-04-25 01:11:37 +0800

    添加 Nav 样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,26 +1,25 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
+  <nav>
+    <router-link to="/money" class="item">
       <Icon name="money"/>
       记账
     </router-link>
-    |
-    <router-link to="/labels">
+    <router-link to="/labels" class="item">
       <Icon name="label"/>
       标签
     </router-link>
-    |
-    <router-link to="/statistics">
+    <router-link to="/statistics" class="item">
       <Icon name="statistics"/>
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
 
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+
 @Component({
   components: {}
 })
@@ -30,7 +29,20 @@ export default class Nav extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.nav {
-  border: 1px solid red;
+nav {
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+
+  > .item {
+    padding: 2px 0;
+    width: 33.33%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    .icon{
+      width: 32px;
+      height: 32px;}
+  }
 }
 </style>
