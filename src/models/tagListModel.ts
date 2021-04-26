35c2a2de48334b49934b0eff4dce77a0e032780d
Mike <15151851516@163.com>
2021-04-26 16:32:08 +0800

    完成 Editlabel 的功能

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,3 +1,5 @@
+import createId from '@/lib/createId';
+
 const localStorageKeyName = 'tagList';
 type Tag = {
     id: string,
@@ -7,6 +9,8 @@ type TagListModel = {
     data: Tag[]
     fetch: () => Tag[]
     create: (name: string) => 'success' | 'duplicated'  //success表示成功，duplicated表示标签名重复
+    update: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
+    remove: (id: string) => boolean
     save: () => void
 }
 
@@ -17,13 +21,42 @@ const tagListModel: TagListModel = {
         return this.data;
     },
     create(name) {
+
         const names = this.data.map(item => item.name);
         if (names.indexOf(name) >= 0) {
             return 'duplicated';
         }
-        this.data.push({id: name, name: name});
+        const id = createId().toString();
+        this.data.push({id: id, name: name});
         this.save();
         return 'success';
+    },
+    update(id, name) {
+        const idList = this.data.map(item => item.id);
+        if (idList.indexOf(id) >= 0) {
+            const names = this.data.map(item => item.name);
+            if (names.indexOf(name) >= 0) {
+                return 'duplicated';
+            } else {
+                const tag = this.data.filter(item => item.id === id)[0];
+                tag.name = name;
+                this.save();
+                return 'success';
+            }
+        } else {
+            return 'not found';
+        }
+    },
+    remove(id: string) {
+        const idList = this.data.map(item => item.id);
+        const index = idList.indexOf(id);
+        if (index >= 0) {
+            this.data.splice(index, 1);
+            this.save();
+            return true;
+        } else {
+            return false;
+        }
     },
     save() {
         localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
