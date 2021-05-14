fbd0d0c7eb30aef031ed065268e8ba561e9b8554
Mike <15151851516@163.com>
2021-05-14 18:40:33 +0800

    修复一进页面就弹出四个提示的bug

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -5,59 +5,69 @@
           :value.sync="record.type"/>
     <div class="notes">
       <FormItem field-name="备注"
+                :value="record.notes"
                 placeholder="在这里输入备注"
                 @update:value="onUpdateNotes"
       />
     </div>
-    <Tags/>
+    <Tags @update:value="record.tags=$event"/>
   </Layout>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import NumberPad from '@/components/Money/NumberPad.vue';
-  import FormItem from '@/components/Money/FormItem.vue';
-  import Tags from '@/components/Money/Tags.vue';
-  import {Component} from 'vue-property-decorator';
-  import Tabs from '@/components/Tabs.vue';
-  import recordTypeList from '@/constants/recordTypeList';
+import Vue from 'vue';
+import NumberPad from '@/components/Money/NumberPad.vue';
+import FormItem from '@/components/Money/FormItem.vue';
+import Tags from '@/components/Money/Tags.vue';
+import {Component} from 'vue-property-decorator';
+import Tabs from '@/components/Tabs.vue';
+import recordTypeList from '@/constants/recordTypeList';
 
-  @Component({
-    components: {Tabs, Tags, FormItem, NumberPad},
-  })
-  export default class Money extends Vue {
-    get recordList() {
-      return this.$store.state.recordList;
+@Component({
+  components: {Tabs, Tags, FormItem, NumberPad},
+})
+export default class Money extends Vue {
+  get recordList() {
+    return this.$store.state.recordList;
+  }
+
+  recordTypeList = recordTypeList;
+
+  // eslint-disable-next-line no-undef
+  record: RecordItem = {
+    tags: [], notes: '', type: '-', amount: 0
+  };
+
+  created() {
+    this.$store.commit('fetchRecords');
+  }
+
+  onUpdateNotes(value: string) {
+    this.record.notes = value;
+  }
+
+  saveRecord() {
+    if (!this.record.tags || this.record.tags.length === 0) {
+      window.alert('请至少选择一个标签');
+      return;
     }
-
-    recordTypeList = recordTypeList;
-
-    // eslint-disable-next-line no-undef
-    record: RecordItem = {
-      tags: [], notes: '', type: '-', amount: 0
-    };
-
-    created() {
-      this.$store.commit('fetchRecords');
-    }
-
-    onUpdateNotes(value: string) {
-      this.record.notes = value;
-    }
-
-    saveRecord() {
-      this.$store.commit('createRecord', this.record);
+    this.$store.commit('createRecord', this.record);
+    if (this.$store.state.createRecordError === null) {
+      window.alert('已保存');
+      this.record.notes = '';
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  ::v-deep .layout-content {
-    display: flex;
-    flex-direction: column-reverse;
-  }
-  .notes {
-    padding: 12px 0;
-  }
+::v-deep .layout-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
+
+.notes {
+  padding: 12px 0;
+}
 </style>
 
